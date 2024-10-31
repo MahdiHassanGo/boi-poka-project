195043ac536c54beeda159d634ea337b500bcb94
MahdiHassanGo <contact.mahdiasif@gmail.com>
2024-11-01 01:16:45 +0600

    pushing dashboard

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">Page Not Found</h2>
+        <p>Error 404</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+import parseRgbLegacy from './../../../node_modules/culori/src/rgb/parseRgbLegacy';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home page </h2>
+        </div>
+    );
+};
+
+export default Home;
